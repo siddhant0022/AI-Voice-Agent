f2292dd0e9adbf6709e9718188273d8f27034192
siddhant0022 <148846164+siddhant0022@users.noreply.github.com>
2025-07-10 02:06:03 +0530

    minor changes

--- app/_components/hero.tsx
+++ app/_components/hero.tsx
@@ -113,14 +113,16 @@ const Navbar = () => {
         <div className="size-7 rounded-full bg-gradient-to-br from-violet-500 to-pink-500" />
         <h1 className="text-base font-bold md:text-2xl">Aceternity UI</h1>
       </div>
-     {!user ? 
+     {!user? 
      <button className="w-24 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 md:w-32 dark:bg-white dark:text-black dark:hover:bg-gray-200">
         Login
       </button> :
+       
     <div>
       <UserButton/>
       </div>}
     </nav>
   );
 };
+
 
